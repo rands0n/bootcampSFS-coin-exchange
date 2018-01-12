21d7f1391a7cfe28f15d8eaf192599b3fe68d60d
Randson Oliveira <orandson@gmail.com>
2018-01-12 10:22:50 -0400

    fix some helper spec error about class name

--- spec/helpers/exchange_helper_spec.rb
+++ spec/helpers/exchange_helper_spec.rb
@@ -10,6 +10,5 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe ExchangeBoxHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ExchangeHelper, type: :helper do
 end
